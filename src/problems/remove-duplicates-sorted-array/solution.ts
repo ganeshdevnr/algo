e7ccd918547f0a941db85a53a13c0242e27d1800
Ganesh <ganesh.dev.nr@gmail.com>
2026-07-03 21:29:16 +0530

    practicing remove duplicate

--- src/problems/remove-duplicates-sorted-array/solution.ts
+++ src/problems/remove-duplicates-sorted-array/solution.ts
@@ -1,13 +1,19 @@
 export default function removeDuplicatesSortedArray(nums: number[]): number {
-  let s = 0;
-  let f = 1;
+  if (nums.length === 0) return 0;
 
-  if (nums.length === 0) return s;
+  let s = 0,
+    f = 1;
+
+  //   while (f < nums.length) {
+  //     if (nums[s] === nums[f]) ++f;
+  //     else {
+  //       nums[++s] = nums[f];
+  //     }
+  //   }
 
   for (; f < nums.length; f++) {
     if (nums[s] !== nums[f]) {
-      ++s;
-      nums[s] = nums[f];
+      nums[++s] = nums[f];
     }
   }
 
