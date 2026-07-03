166947a772bc9339ff080083627e3eade28caaf1
Ganesh <ganesh.dev.nr@gmail.com>
2026-07-03 17:41:48 +0530

    bubble sort implemented

--- src/problems/bubble-sort/solution.ts
+++ src/problems/bubble-sort/solution.ts
@@ -1,3 +1,15 @@
 export default function bubbleSort(arr: number[]): number[] {
-  throw new Error("Not implemented");
+  let sortedArray: number[] = [...arr];
+
+  for (let i = 0; i < sortedArray.length; i++) {
+    for (let j = 0; j < sortedArray.length - i - 1; j++) {
+      if (sortedArray[j] > sortedArray[j + 1]) {
+        const temp = sortedArray[j];
+        sortedArray[j] = sortedArray[j + 1];
+        sortedArray[j + 1] = temp;
+      }
+    }
+  }
+
+  return sortedArray;
 }
